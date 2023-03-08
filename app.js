8bbbc333e17c1dfc2c5aab1e2a0e18481a2b612e
Shubham04Sanap <shubhamsanap0705@gmail.com>
2023-03-08 18:56:18 +0530

    js file added with eventListener

---
+++ app.js
@@ -0,0 +1,6 @@
+let inputText = document.querySelector("#input-Text");
+let translatedText = document.addEventListener("click", clickHandler());
+
+function clickHandler(){
+  console.log(inputText);
+}
